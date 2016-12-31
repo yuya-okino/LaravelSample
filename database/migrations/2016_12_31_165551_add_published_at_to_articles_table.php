866018f133de03af8e146f5a51fe00ac6b756970
yuya-okino <okinoy2312@gmail.com>
2016-12-31 17:04:58 +0900

    database

---
+++ database/migrations/2016_12_31_165551_add_published_at_to_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPublishedAtToArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+            $table->dropColumn('published_at');
+        });
+    }
+}
